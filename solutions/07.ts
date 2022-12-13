a4e87083c26d8221dc505606f7fc1a56672855b7
Bobby David <dartkietanmartaru@gmail.com>
2022-12-12 22:02:23 -0500

    day 8

--- solutions/07.ts
+++ solutions/07.ts
@@ -1,69 +1,98 @@
 import { readRawInput, rotateClockwise } from '../helpers/';
 
 const input: string = readRawInput('07');
+let highestScenicScore = 0;
 
 (function someTallFuckingTrees() {
     let trees = buildTreeGrid(input);
-    let visibleTrees = ((trees.length * 2) + (trees[0].length * 2)) - 4;
-    
-    for(let row = 1; row < trees.length - 1; row++){
-        for(let col = 1; col < trees[row].length - 1; col++){
-            if(isTreeVisible(row,col,trees)){
+    let visibleTrees = 0;
+
+    for (let row = 0; row < trees.length; row++) {
+        for (let col = 0; col < trees[row].length; col++) {
+            if (isTreeVisible(row, col, trees)) {
                 visibleTrees++;
             }
         }
     }
 
     console.log(`Visible Tree Count: ${visibleTrees}`);
+    console.log(`Highest Scenic Score: ${highestScenicScore}`);
 })();
 
-function isTreeVisible(rowIndex: number, colIndex: number, trees: number[][]): boolean{
-    if(isEdgeTree(rowIndex,colIndex,trees)) return true;
+function isTreeVisible(rowIndex: number, colIndex: number, trees: number[][]): boolean {
+    const tree = trees[rowIndex][colIndex];
 
-    let left = scanLeft(rowIndex,colIndex,trees);
-    let right = scanRight(rowIndex,colIndex,trees);
-    let down = scanDown(rowIndex,colIndex,trees);
-    let up = scanUp(rowIndex,colIndex,trees);
+    let left = scanLeft(rowIndex, colIndex, trees, tree);
+    let right = scanRight(rowIndex, colIndex, trees, tree);
+    let down = scanDown(rowIndex, colIndex, trees, tree);
+    let up = scanUp(rowIndex, colIndex, trees, tree);
 
-    return left || right || down || up;
+    let score = left.treeCount * right.treeCount * up.treeCount * down.treeCount;
+    highestScenicScore = Math.max(highestScenicScore, score);
+
+    return isEdge(rowIndex,colIndex,trees) || left.isTallest || right.isTallest || down.isTallest || up.isTallest;
 }
 
-function scanLeft(rowIndex: number, colIndex: number, trees: number[][]){
-    for(let index = colIndex -1; index >= 0; index--){
-        if(trees[rowIndex][index] >= trees[rowIndex][colIndex])
-            return false;
+function scanLeft(rowIndex: number, colIndex: number, trees: number[][], tree: number) {
+    let isTallest = true;
+    let treeCount = 0;
+    for (let index = colIndex - 1; index >= 0; index--) {
+        treeCount++;
+        isTallest = isTreeTaller(tree, trees[rowIndex][index])
+        if (!isTallest){            
+            break;
+        }
     }
-    return true;
+    return {isTallest,treeCount}
 }
 
-function scanRight(rowIndex: number, colIndex: number, trees: number[][]){
-    for(let index = colIndex + 1; index <= trees[rowIndex].length; index++){
-        if(trees[rowIndex][index] >= trees[rowIndex][colIndex])
-            return false;
+function scanRight(rowIndex: number, colIndex: number, trees: number[][], tree: number) {
+    let isTallest = true;
+    let treeCount = 0;
+    for (let index = colIndex + 1; index < trees[rowIndex].length; index++) {
+        treeCount++;
+        isTallest = isTreeTaller(tree, trees[rowIndex][index])
+        if (!isTallest){            
+            break;
+        }
     }
-    return true;
+    return {isTallest,treeCount}
 }
 
-function scanDown(rowIndex: number, colIndex: number, trees: number[][]){
-    for(let index = rowIndex -1; index <= trees[rowIndex].length; index++){
-        if(trees[index][colIndex] >= trees[rowIndex][colIndex])
-            return false;
+function scanDown(rowIndex: number, colIndex: number, trees: number[][], tree: number) {
+    let isTallest = true;
+    let treeCount = 0;
+    for (let index = rowIndex + 1; index < trees[rowIndex].length; index++) {
+        treeCount++;
+        isTallest = isTreeTaller(tree, trees[index][colIndex])
+        if (!isTallest){            
+            break;
+        }
     }
-    return true;
+    return {isTallest,treeCount}
 }
 
-function scanUp(rowIndex: number, colIndex: number, trees: number[][]){
-    for(let index = rowIndex -1; index >= 0; index--){
-        if(trees[index][colIndex] >= trees[rowIndex][colIndex])
-            return false;
+function scanUp(rowIndex: number, colIndex: number, trees: number[][], tree: number) {
+    let isTallest = true;
+    let treeCount = 0;
+    for (let index = rowIndex - 1; index >= 0; index--) {
+        treeCount++;
+        isTallest = isTreeTaller(tree, trees[index][colIndex])
+        if (!isTallest){            
+            break;
+        }
     }
-    return true;
+    return {isTallest,treeCount}
 }
 
-function buildTreeGrid(input: string):number[][] {
-    return input.split('\n').map( trees => trees.split('').map(Number) );
+function isTreeTaller(tree: number, currentNeighbor: number){
+    return currentNeighbor >= tree;
 }
 
-function isEdgeTree(row:number,col:number,trees:number[][]): boolean {
-    return (!row || !col || ( row === trees.length -1 ) || ( col === trees[row].length -1 ) )
+function buildTreeGrid(input: string): number[][] {
+    return input.split('\n').map(trees => trees.split('').map(Number));
+}
+
+function isEdge(rowIndex: number, colIndex: number, trees: number[][]){
+    return !rowIndex || !colIndex || rowIndex === trees.length -1 || colIndex === trees[0].length -1;
 }
